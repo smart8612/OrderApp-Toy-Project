764bbe1d4976c20b74d62fe0ea56c124b10da58b
JeongTaek Han <smart8612@gmail.com>
2023-01-28 01:26:48 +0900

    feat: implement restaurant controller #2

---
+++ OrderApp/Model controllers/RestaurantController.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantController.swift
+//  OrderApp
+//
+//  Created by JeongTaek Han on 2023-01-28.
+//
+
+import Foundation
+
+class RestaurantController {
+    typealias MinutesToPrepare = Int
+    
+    private let networkController = NetworkController()
+    
+    func submitOrder(forMenuIDs menuIDs: [Int]) async throws -> MinutesToPrepare {
+        let apiRequest = RestaurantOrderPostAPIRequest(menuIDs: menuIDs)
+        let result = try await networkController.send(request: apiRequest)
+        return result.preperationTime
+    }
+    
+    func fetchCategories() async throws -> [String] {
+        let apiRequest = RestaurantCategoriesGetAPIRequest()
+        let result = try await networkController.send(request: apiRequest)
+        return result.categories
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String) async throws -> [MenuItem] {
+        let apiRequest = RestaurantMenuItemsGetAPIRequest(categoryName: categoryName)
+        let result = try await networkController.send(request: apiRequest)
+        return result.items
+    }
+}
