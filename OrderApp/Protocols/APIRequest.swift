1816c005c3841b1e1e639317613e5522c697d629
JeongTaek Han <smart8612@gmail.com>
2023-01-28 00:51:49 +0900

    feat: implement postJsonURLRequest method #2
    utility function

--- OrderApp/Protocols/APIRequest.swift
+++ OrderApp/Protocols/APIRequest.swift
@@ -10,9 +10,24 @@ import Foundation
 protocol APIRequest {
     associatedtype Response
     
+    var baseURL: URL? { get }
     var urlRequest: URLRequest? { get }
     func decodeResponse(data: Data) throws -> Response
     
     @discardableResult
     func verify(response: URLResponse) throws -> Bool
+}
+
+extension APIRequest {
+    var postJsonURLRequest: URLRequest? {
+        guard let baseURL = baseURL else {
+            return nil
+        }
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "POST"
+        request.setValue(
+            "application/json",
+            forHTTPHeaderField: "Content-Type"
+        )
+    }
 }
