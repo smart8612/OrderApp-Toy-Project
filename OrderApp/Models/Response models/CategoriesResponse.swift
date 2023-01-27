1dd9f6561896d049af722fd04f471b8dbe81ac43
JeongTaek Han <smart8612@gmail.com>
2023-01-27 15:58:11 +0900

    feat: define response model objects #2
    define menu, categories, order response model object on Response models directory

---
+++ OrderApp/Models/Response models/CategoriesResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CategoriesResponse.swift
+//  OrderApp
+//
+//  Created by JeongTaek Han on 2023-01-27.
+//
+
+import Foundation
+
+struct CategoriesResponse: Codable {
+    let categories: [String]
+}
---
+++ OrderApp/Models/Response models/MenuResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MenuResponse.swift
+//  OrderApp
+//
+//  Created by JeongTaek Han on 2023-01-27.
+//
+
+import Foundation
+
+struct MenuResponse: Codable {
+    let items: [MenuItem]
+}
