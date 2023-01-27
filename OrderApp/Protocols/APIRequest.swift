e85adbd9b5d6e541617c48c7441673d08ce4246d
JeongTaek Han <smart8612@gmail.com>
2023-01-27 23:09:06 +0900

    refactor: change APIRequest's urlRequest to optional
    because if provided url string is invalid than can not make URL and URLRequest

--- OrderApp/Protocols/APIRequest.swift
+++ OrderApp/Protocols/APIRequest.swift
@@ -10,6 +10,6 @@ import Foundation
 protocol APIRequest {
     associatedtype Response
     
-    var urlRequest: URLRequest { get }
+    var urlRequest: URLRequest? { get }
     func decodeResponse(data: Data) throws -> Response
 }
