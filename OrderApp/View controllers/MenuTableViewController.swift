0c635e8a9344d3b4e299072f6d5d0acc533e9851
JeongTaek Han <smart8612@gmail.com>
2023-01-31 15:41:46 +0900

    feat: configure tableview cell for load image #3

--- OrderApp/View controllers/MenuTableViewController.swift
+++ OrderApp/View controllers/MenuTableViewController.swift
@@ -13,6 +13,7 @@ class MenuTableViewController: UITableViewController {
     private let restaurantController = RestaurantController.shared
     private var menuItems: [MenuItem] = []
     private let category: String
+    private var imageLoadTasks: [IndexPath:Task<Void, Never>] = [:]
     
     init?(coder: NSCoder, category: String) {
         self.category = category
@@ -21,6 +22,14 @@ class MenuTableViewController: UITableViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        imageLoadTasks.forEach { (key: IndexPath, value: Task<Void, Never>) in
+            value.cancel()
+        }
     }
     
     override func viewDidLoad() {
@@ -68,6 +77,10 @@ extension MenuTableViewController {
         return menuItems.count
     }
     
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        imageLoadTasks[indexPath]?.cancel()
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItem", for: indexPath)
         configure(cell, forItemAt: indexPath)
@@ -75,13 +88,27 @@ extension MenuTableViewController {
     }
     
     private func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? MenuItemTableViewCell else {
+            return
+        }
+        
         let menuItem = menuItems[indexPath.row]
         
-        var contentConfiguration = cell.defaultContentConfiguration()
-        contentConfiguration.text = menuItem.name
-        contentConfiguration.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
-        contentConfiguration.image = UIImage(systemName: "photo.on.rectangle")
-        cell.contentConfiguration = contentConfiguration
+        cell.itemName = menuItem.name
+        cell.price = menuItem.price
+        cell.image = nil
+        
+        imageLoadTasks[indexPath] = Task {
+            if let data = try? await restaurantController.fetchImage(from: menuItem.imageURL),
+               let image = UIImage(data: data),
+               let currentIndexPath = self.tableView.indexPath(for: cell) {
+                cell.image = (currentIndexPath == indexPath) ? image:nil
+            } else {
+                cell.image = nil
+            }
+            
+            imageLoadTasks[indexPath] = nil
+        }
     }
     
 }
---
+++ OrderApp/View/MenuItemTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  MenuItemTableViewCell.swift
+//  OrderApp
+//
+//  Created by JeongTaek Han on 2023-01-31.
+//
+
+import UIKit
+
+class MenuItemTableViewCell: UITableViewCell {
+    
+    var itemName: String? = nil {
+        didSet {
+            if oldValue != itemName {
+                setNeedsUpdateConfiguration()
+            }
+        }
+    }
+    
+    var price: Double? = nil {
+        didSet {
+            if oldValue != price {
+                setNeedsUpdateConfiguration()
+            }
+        }
+    }
+    
+    var image: UIImage? = nil {
+        didSet {
+            if oldValue != image {
+                setNeedsUpdateConfiguration()
+            }
+        }
+    }
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        var contentConfiguration = defaultContentConfiguration().updated(for: state)
+        contentConfiguration.text = itemName
+        contentConfiguration.secondaryText = price?.formatted(.currency(code: "usd"))
+        contentConfiguration.image = (image == nil) ? UIImage(systemName: "photo.on.rectangle") : image
+        self.contentConfiguration = contentConfiguration
+    }
+}
