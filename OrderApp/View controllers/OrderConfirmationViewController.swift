e28084706315eba0a86263a5f45600d10c32c529
JeongTaek Han <smart8612@gmail.com>
2023-02-02 17:00:04 +0900

    feat: define UIProgressiveView to visually describe remaining time #6

--- OrderApp/View controllers/OrderConfirmationViewController.swift
+++ OrderApp/View controllers/OrderConfirmationViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 final class OrderConfirmationViewController: UIViewController {
     
     private let minutesToPrepare: Int
+    private let startingOrderDate = Date()
+    private var timeIntervalToPrepare: TimeInterval {
+        TimeInterval(minutesToPrepare)
+    }
+    
     private let restaurantController = RestaurantController.shared
     
     @IBOutlet private weak var confirmationLabel: UILabel?
+    @IBOutlet private weak var timeProgressiveView: UIProgressView!
+    
+    private var scheduleTimer: Timer?
     
     required init?(coder: NSCoder, minutesToPrepare: Int) {
         self.minutesToPrepare = minutesToPrepare
@@ -23,13 +31,39 @@ final class OrderConfirmationViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        scheduleTimer?.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureProgressiveBarUpdateSchedule()
         updateUI()
     }
     
     private func updateUI() {
         confirmationLabel?.text = "Thank you for your order! Your wait time is approximately \(minutesToPrepare) minutes."
+        timeProgressiveView?.setProgress(0.0, animated: true)
+    }
+    
+    private func configureProgressiveBarUpdateSchedule() {
+        scheduleTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+            guard var timeIntervalFromStartingOrder = self?.startingOrderDate.timeIntervalSinceNow,
+                  let timeIntervalToPrepare = self?.timeIntervalToPrepare else {
+                self?.scheduleTimer?.invalidate()
+                return
+            }
+            
+            timeIntervalFromStartingOrder = abs(timeIntervalFromStartingOrder)
+            
+            if timeIntervalFromStartingOrder >= timeIntervalToPrepare {
+                self?.timeProgressiveView.setProgress(1.0, animated: true)
+                self?.scheduleTimer?.invalidate()
+            } else {
+                let computeDateRatio = Float(timeIntervalFromStartingOrder / timeIntervalToPrepare)
+                self?.timeProgressiveView?.setProgress(computeDateRatio, animated: true)
+            }
+        }
     }
     
 }
