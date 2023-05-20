6a862512e89f893785cade5a68976791605c6bbd
JeongTaek Han <smart8612@gmail.com>
2023-05-20 14:50:11 +0900

    refactor: add dependency "UserNotificationKit"
    remove associated source code on this project

--- OrderApp/Models/Notification models/OrderCompleteNotificationCategory.swift
+++ OrderApp/Models/Notification models/OrderCompleteNotificationCategory.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UserNotifications
+import UserNotificationKit
 
 struct OrderCompleteNotificationCategory: NotificationCategory {
     
--- OrderApp/View models/OrderConfirmationViewModel.swift
+++ OrderApp/View models/OrderConfirmationViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import UserNotificationKit
 
 final class OrderConfirmationViewModel: ObservableObject {
     
